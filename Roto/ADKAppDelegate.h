9ead63088600ef6d3bbf767d5efbc32caaf0e3a3
Rob Stenson <rob.stenson@gmail.com>
2012-11-15 23:36:35 -0800

    Initial Commit

---
+++ Roto/ADKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ADKAppDelegate.h
+//  Roto
+//
+//  Created by Robert Stenson on 11/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ADKViewController;
+
+@interface ADKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ADKViewController *viewController;
+
+@end
---
+++ Roto/ADKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADKViewController.h
+//  Roto
+//
+//  Created by Robert Stenson on 11/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADKViewController : UIViewController
+
+@end
